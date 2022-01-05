21dc868a2b3fa415d567ab056b16976144c7db9f
Erik Westrup <erik.westrup@gmail.com>
2022-01-05 17:27:57 +0100

    Read directory names

--- api/nestedtext_official_tests.rb
+++ api/nestedtext_official_tests.rb
@@ -2,6 +2,22 @@ module NestedTextOfficialTests
   extend self
 
   def load_test_cases
-    %w[0 1111111 2222222]
+    official_dir = File.expand_path(File.dirname(__FILE__) + "/..")
+    cases_dir = official_dir + "/test_cases"
+
+    cases = []
+    Dir.each_child(cases_dir) do |case_dir|
+      cases << TestCase.new(case_dir)
+    end
+
+    cases
+  end
+
+  class TestCase
+    attr_reader :name
+
+    def initialize(name)
+      @name = name
+    end
   end
 end
